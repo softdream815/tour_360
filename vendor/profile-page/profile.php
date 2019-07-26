b1085c291263e5ab0b9175d5710456dacf597728
softdream815 <uyg0815@gmail.com>
2019-07-26 20:14:55 +0545

    Update README.md

--- vendor/profile-page/profile.php
+++ vendor/profile-page/profile.php
@@ -8,7 +8,7 @@ include '../dbConfig.php';
 <head>
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title> Mt. Everest Profile</title>
+    <title> Profile</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
 
